2cf18e546b75cc99f0d421e13cc1d81a63356f06
Miguel Luis <miguelluisataideferreira68@gmail.com>
2024-10-03 14:41:24 -0300

    first commit

---
+++ src/shared/components/header.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+type Props = {}
+
+export const Header = (props: Props) => {
+  return (
+    <header>
+
+    </header>
+  )
+}
---
+++ src/shared/fonts/popins.ts
@@ -0,0 +1,6 @@
+import { Poppins } from "next/font/google"
+
+export const popins = Poppins({
+  subsets: ["latin"],
+  weight: ["200", "400", "500", "600", "700", "800", "900"],
+})
