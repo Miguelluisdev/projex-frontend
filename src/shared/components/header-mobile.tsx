23491239b2d23218ff652edce51687224c6eb27b
Miguel Luis <miguelluisataideferreira68@gmail.com>
2024-10-03 20:00:26 -0300

    feat: create header component

---
+++ src/shared/components/header-mobile.tsx
@@ -0,0 +1,41 @@
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet"
+import { Menu } from "lucide-react"
+
+type Props = {}
+
+export const HeaderMobile = (props: Props) => {
+  return (
+    <Sheet>
+      <SheetTrigger>
+        <Menu className="w-10 h-10 md:hidden" />
+      </SheetTrigger>
+      <SheetContent className="bg-black border-none">
+        <SheetDescription>
+          <div className="">
+            <nav className="flex mt-40 flex-col justify-between items-center gap-8 text-sm ">
+              <a href="" className="text-white/70 hover:text-white transition">
+                Sobre
+              </a>
+              <a href="" className="text-white/70 hover:text-white transition">
+                Avaliações
+              </a>
+              <a href="" className="text-white/70 hover:text-white transition">
+                Desenvolvedores
+              </a>
+              <a href="" className="text-white/70 hover:text-white transition">
+                Projetos
+              </a>
+            </nav>
+          </div>
+        </SheetDescription>
+      </SheetContent>
+    </Sheet>
+  )
+}
--- src/shared/components/header.tsx
+++ src/shared/components/header.tsx
@@ -1,11 +1,56 @@
-import React from "react"
+import { Menu } from "lucide-react"
+import Image from "next/image"
+import { HeaderMobile } from "./header-mobile"
 
-type Props = {}
-
-export const Header = (props: Props) => {
+export const Header = () => {
   return (
-    <header>
+    <header className="py-4 border-b border-white/15 md:border-none">
+      <div className="container">
+        <div className="flex justify-between items-center md:border border-white/15 md:p-2.5 rounded-xl max-w-2xl mx-auto">
+          <div>
+            <div className="border h-10 w-10 rounded-lg inline-flex  justify-center border-white/15">
+              <Image
+                src="/project-management.png"
+                alt=""
+                width={50}
+                height={50}
+              />
+            </div>
+          </div>
 
+          <div className="hidden md:block">
+            <nav className="flex gap-8 text-sm ">
+              <a href="" className="text-white/70 hover:text-white transition">
+                Sobre
+              </a>
+              <a href="" className="text-white/70 hover:text-white transition">
+                Avaliações
+              </a>
+              <a href="" className="text-white/70 hover:text-white transition">
+                Desenvolvedores
+              </a>
+              <a href="" className="text-white/70 hover:text-white transition">
+                Projetos
+              </a>
+            </nav>
+          </div>
+
+          <div className="flex gap-2 items-center">
+            <button className=" relative border py-2 px-3 rounded-lg font-medium text-sm bg-gradient-to-b from-[#190d2e] to-[#4a208a] shadow-[0px_0px_12px_#8c45ff]">
+              <div className="absolute inset-0 ">
+                <div className=" rounded-lgborder border-white/20 absolute inset-0 rounded-lg [mask-image:linear-gradient(to_bottom,black,transparent)]"></div>
+                <div className="rounded-lg  border absolute inset-0 border-white/40  [mask-image:linear-gradient(to_top,black,transparent)]"></div>
+                <div
+                  className="absolute inset-0 shadow-[0_0_10px_rgb(140,69,255,.7)_inset] rounded-lg
+                 "
+                ></div>
+              </div>
+              <span>Entrar</span>
+            </button>
+            <HeaderMobile />
+          </div>
+        </div>
+      </div>
     </header>
   )
 }
