7add6a123c073fbec411798946f156382c927ae0
Miguel Luis <miguelluisataideferreira68@gmail.com>
2024-10-04 15:53:36 -0300

    feat: create a custom button

--- src/shared/components/header.tsx
+++ src/shared/components/header.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image"
 import { HeaderMobile } from "./header-mobile"
+import { Button } from "./button"
 
 export const Header = () => {
   return (
@@ -35,17 +36,7 @@ export const Header = () => {
           </div>
 
           <div className="flex gap-2 items-center">
-            <button className=" relative border py-2 px-3 rounded-lg font-medium text-sm bg-gradient-to-b from-[#190d2e] to-[#4a208a] shadow-[0px_0px_12px_#8c45ff]">
-              <div className="absolute inset-0 ">
-                <div className=" rounded-lgborder border-white/20 absolute inset-0 rounded-lg [mask-image:linear-gradient(to_bottom,black,transparent)]"></div>
-                <div className="rounded-lg  border absolute inset-0 border-white/40  [mask-image:linear-gradient(to_top,black,transparent)]"></div>
-                <div
-                  className="absolute inset-0 shadow-[0_0_10px_rgb(140,69,255,.7)_inset] rounded-lg
-                 "
-                ></div>
-              </div>
-              <span>Entrar</span>
-            </button>
+           <Button words="Entrar" />
             <HeaderMobile />
           </div>
         </div>
---
+++ src/subdomains/home/hero.tsx
@@ -0,0 +1,17 @@
+import { Button } from "@/shared/components/button"
+
+export const Hero = () => {
+  return (
+    <section>
+      <div className="container">
+        <h1>PROJEX</h1>
+        <p>
+          Mantenha suas ideias e projetos sob controle com nossa ferramenta de
+          gestão personalizada. Crie, planeje, acompanhe e conclua suas tarefas
+          com facilidade e praticidade, tudo em um só lugar.
+        </p>
+        <Button words="Comece agora" />
+      </div>
+    </section>
+  )
+}
