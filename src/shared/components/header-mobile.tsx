2dc8c340b214cbbbf25f18ad4530540d38b64d2c
Miguel Luis <miguelluisataideferreira68@gmail.com>
2024-10-07 17:38:08 -0300

    styles: change styles from header

--- src/shared/components/header-mobile.tsx
+++ src/shared/components/header-mobile.tsx
@@ -2,34 +2,34 @@ import {
   Sheet,
   SheetContent,
   SheetDescription,
-
   SheetTrigger,
 } from "@/components/ui/sheet"
 import { Menu } from "lucide-react"
-
+import { Buttons } from "./button"
 
 export const HeaderMobile = () => {
   return (
     <Sheet>
       <SheetTrigger>
-        <Menu className="w-10 h-10 md:hidden" />
+        <Menu className="h-8 w-8 md:hidden" />
       </SheetTrigger>
-      <SheetContent className="bg-black border-none">
+      <SheetContent className="bg-[#EAEEFE] border-none">
         <SheetDescription>
           <div className="">
             <nav className="flex mt-40 flex-col justify-between items-center gap-8 text-sm ">
-              <a href="" className="text-white/70 hover:text-white transition">
+              <a href="" className="text-black hover:text-[#1F3473] transition">
                 Sobre
               </a>
-              <a href="" className="text-white/70 hover:text-white transition">
+              <a href="" className="text-black hover:text-[#1F3473] transition">
                 Avaliações
               </a>
-              <a href="" className="text-white/70 hover:text-white transition">
+              <a href="" className="text-black hover:text-[#1F3473] transition">
                 Desenvolvedores
               </a>
-              <a href="" className="text-white/70 hover:text-white transition">
+              <a href="" className="text-black hover:text-[#1F3473] transition">
                 Projetos
               </a>
+              <Buttons words="Entrar" />
             </nav>
           </div>
         </SheetDescription>
--- src/shared/components/header.tsx
+++ src/shared/components/header.tsx
@@ -1,46 +1,65 @@
+import { ArrowRight, Menu, MoveRight } from "lucide-react"
 import Image from "next/image"
+import Link from "next/link"
+import { Buttons } from "./button"
 import { HeaderMobile } from "./header-mobile"
-import { Button } from "./button"
 
 export const Header = () => {
   return (
-    <header className="py-4 border-b border-white/15 md:border-none">
-      <div className="container">
-        <div className="flex justify-between items-center md:border border-white/15 md:p-2.5 rounded-xl max-w-2xl mx-auto">
-          <div>
-            <div className="border h-10 w-10 rounded-lg inline-flex  justify-center border-white/15">
-              <Image
-                src="/project-management.png"
-                alt=""
-                width={50}
-                height={50}
-              />
+    <>
+      <header className="sticky top-0">
+        <div className="flex justify-center items-center py-3 bg-black text-white text-sm">
+          <div className="inline-flex gap-1 items-center"></div>
+          <p>
+            {" "}
+            <span className="text-white/40">Gerencie seus projetos</span>{" "}
+            totalmente gratís{" "}
+          </p>
+          <ArrowRight className="h-4 w-4 ml-2 inline-flex justify-center items-center" />
+        </div>
+        <nav className="py-5">
+          <div className="container">
+            <div className="flex items-center justify-between">
+              <Link rel="stylesheet" href="/">
+                <Image
+                  src="/project-management.png"
+                  alt="logo do site"
+                  width={40}
+                  height={40}
+                />
+              </Link>
+              <HeaderMobile />
+              <nav className="hidden md:flex gap-6 text-black/60 items-center">
+                <a
+                  href=""
+                  className="text-black/70 hover:text-[#1F3473] transition"
+                >
+                  Sobre
+                </a>
+                <a
+                  href=""
+                  className="text-black/70 hover:text-[#1F3473] transition"
+                >
+                  Avaliações
+                </a>
+                <a
+                  href=""
+                  className="text-black/70 hover:text-[#1F3473] transition"
+                >
+                  Desenvolvedores
+                </a>
+                <a
+                  href=""
+                  className="text-black/70 hover:text-[#1F3473] transition"
+                >
+                  Projetos
+                </a>
+                <Buttons words="Entrar" />
+              </nav>
             </div>
           </div>
-
-          <div className="hidden md:block">
-            <nav className="flex gap-8 text-sm ">
-              <a href="" className="text-white/70 hover:text-white transition">
-                Sobre
-              </a>
-              <a href="" className="text-white/70 hover:text-white transition">
-                Avaliações
-              </a>
-              <a href="" className="text-white/70 hover:text-white transition">
-                Desenvolvedores
-              </a>
-              <a href="" className="text-white/70 hover:text-white transition">
-                Projetos
-              </a>
-            </nav>
-          </div>
-
-          <div className="flex gap-2 items-center">
-           <Button words="Entrar" />
-            <HeaderMobile />
-          </div>
-        </div>
-      </div>
-    </header>
+        </nav>
+      </header>
+    </>
   )
 }
