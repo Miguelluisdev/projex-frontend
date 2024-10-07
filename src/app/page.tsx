c5403a34825e353577a435e34049da416a72ff34
Miguel Luis <miguelluisataideferreira68@gmail.com>
2024-10-07 18:58:00 -0300

    feat: add herosection from home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import { Header } from "@/shared/components/header"
+import HeroSection from "@/subdomains/home/hero"
 import Image from "next/image"
 
 export default function Home() {
   return (
     <main className="">
       <Header />
+      <HeroSection />
     </main>
   )
 }
