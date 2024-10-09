811788a504cda0fda43051d69cfcc59cb899935b
Miguel Luis <miguelluisataideferreira68@gmail.com>
2024-10-09 18:13:57 -0300

    feat: create a logo ticker section

---
+++ src/shared/data/logo-ticker-data.ts
@@ -0,0 +1,22 @@
+export const logos = [
+  {
+    name: "Gerencie as task",
+    img: "/task-actions.png",
+  },
+  {
+    name: "Gerencie o tempo",
+    img: "/deadline.png",
+  },
+  {
+    name: "Gerencie os integrantes",
+    img: "/delegate.png",
+  },
+  {
+    name: "Gerencie os projetos",
+    img: "/project-management1.png",
+  },
+  {
+    name: "Gerencie os pacotes",
+    img: "/product-management.png",
+  },
+]
