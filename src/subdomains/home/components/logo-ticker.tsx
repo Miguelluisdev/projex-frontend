d81248470239acba4ecc9d4f81b29f40f81bf8fb
Miguel Luis <miguelluisataideferreira68@gmail.com>
2024-10-09 18:14:30 -0300

    feat: create a logo ticker action

---
+++ src/subdomains/home/components/logo-ticker.tsx
@@ -0,0 +1,42 @@
+"use client"
+import { logos } from "@/shared/data/logo-ticker-data"
+import { motion } from "framer"
+import Image from "next/image"
+
+const LogoTicker = () => {
+  return (
+    <section className="py-5 bg-white md:py-9">
+      <div className="container">
+        <div className="flex overflow-hidden [mask-image:linear-gradient(to_right,transparent,black,transparent)]">
+          <motion.div
+            className="flex gap-14 flex-none pr-14"
+            animate={{
+              translateX: "-50%",
+            }}
+            transition={{
+              duration: 5,
+              repeat: Infinity,
+              ease: "linear",
+              repeatType: "loop"
+            }}
+          >
+            {logos.map((logo, index) => (
+              <div key={index} className="flex items-center gap-4">
+                <Image
+                  src={logo.img}
+                  alt={logo.name}
+                  width={50}
+                  height={50}
+                  className="h-10 w-10"
+                />
+                <span>{logo.name}</span>
+              </div>
+            ))}
+          </motion.div>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default LogoTicker
