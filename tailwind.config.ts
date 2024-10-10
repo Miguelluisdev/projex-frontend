14cd23e7bf0e6736e57909dfc75494b2a1ec32b6
Miguel Luis <miguelluisataideferreira68@gmail.com>
2024-10-10 20:45:04 -0300

    chore: add dependencies

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,27 +9,52 @@ const config: Config = {
     "./src/**/*.{ts,tsx}",
   ],
   theme: {
-    container: {
-      center: true,
-     
-      screens: {
-        sm: "375px",
-        md: "768px",
-        lg: "1200px",
-      },
-    },
-    screens: { sm: "375px", md: "768px", lg: "1200px" },
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-      borderRadius: {
-        lg: "var(--radius)",
-        md: "calc(var(--radius) - 2px)",
-        sm: "calc(var(--radius) - 4px)",
-      },
-    },
+  	container: {
+  		center: 'true',
+  		screens: {
+  			sm: '375px',
+  			md: '768px',
+  			lg: '1200px'
+  		}
+  	},
+  	screens: {
+  		sm: '375px',
+  		md: '768px',
+  		lg: '1200px'
+  	},
+  	extend: {
+  		colors: {
+  			background: 'var(--background)',
+  			foreground: 'var(--foreground)'
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		},
+  		keyframes: {
+  			'accordion-down': {
+  				from: {
+  					height: '0'
+  				},
+  				to: {
+  					height: 'var(--radix-accordion-content-height)'
+  				}
+  			},
+  			'accordion-up': {
+  				from: {
+  					height: 'var(--radix-accordion-content-height)'
+  				},
+  				to: {
+  					height: '0'
+  				}
+  			}
+  		},
+  		animation: {
+  			'accordion-down': 'accordion-down 0.2s ease-out',
+  			'accordion-up': 'accordion-up 0.2s ease-out'
+  		}
+  	}
   },
   plugins: [require("tailwindcss-animate")],
 }
