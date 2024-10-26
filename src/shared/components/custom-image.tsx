9ba668d264426aa1ba864c9bef53bceab4a577de
Miguel Luis <miguelluisataideferreira68@gmail.com>
2024-10-25 21:39:54 -0300

    feat: create custom image for all app

---
+++ src/shared/components/custom-image.tsx
@@ -0,0 +1,34 @@
+import { ClassValue } from "clsx"
+import Image from "next/image"
+import React from "react"
+import { cn } from "../lib/utils"
+
+interface CustomImageProps
+  extends React.DetailedHTMLProps<
+    React.ImgHTMLAttributes<HTMLImageElement>,
+    HTMLImageElement
+  > {
+  src: string
+  width: number
+  height: number
+  quality?: number
+  className: string
+  alt: string
+}
+
+const CustomImage = (props: CustomImageProps) => {
+  return (
+    <div>
+      <Image
+        src={props.src}
+        width={props.width}
+        height={props.height}
+        quality={props.quality}
+        alt={props.alt}
+        className={cn("", props.className)}
+      />
+    </div>
+  )
+}
+
+export default CustomImage
