7802fecd83f6619f68524e739853fa731cf552de
Miguel Luis <miguelluisataideferreira68@gmail.com>
2024-10-29 22:06:44 -0300

    feat: init dashboard and add sidebar from shadcn

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,10 @@
+import SideBar from "@/subdomains/dashboard/components/sidebar"
+const Dashboard = () => {
+  return (
+    <main className="w-screen h-screen flex flex-col">
+      <SideBar />
+    </main>
+  )
+}
+
+export default Dashboard
