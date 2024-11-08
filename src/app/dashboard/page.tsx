4fbd867cafd08568aa99cab2d08f32aafc2e3559
Miguel Luis <miguelluisataideferreira68@gmail.com>
2024-11-07 21:37:29 -0300

    refactor: put sidebar in layout.tsx

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,8 +1,8 @@
-import SideBar from "@/subdomains/dashboard/components/sidebar"
+
 const Dashboard = () => {
   return (
     <main className="w-screen h-screen flex flex-col">
-      <SideBar />
+    
     </main>
   )
 }
