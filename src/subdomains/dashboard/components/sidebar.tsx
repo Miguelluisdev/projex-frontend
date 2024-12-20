bdd92e8684bf1defc805b8b67f06382d3888ff45
Miguel Luis <miguelluisataideferreira68@gmail.com>
2024-12-20 02:42:55 -0300

    remove: project archive

--- src/subdomains/dashboard/components/sidebar.tsx
+++ src/subdomains/dashboard/components/sidebar.tsx
@@ -13,7 +13,6 @@ import SideDropdown from "./side-dropdown"
 import SideFooter from "./side-footer"
 import SideInset from "./side-inset"
 import SidePlataform from "./side-plataform"
-import SideProjects from "./side-projects"
 import SideShortCutScreen from "./side-shortcut-screen"
 
 export default function SideBar() {
@@ -30,7 +29,6 @@ export default function SideBar() {
         <SidebarContent>
           <SidePlataform />
           <SideShortCutScreen />
-          <SideProjects />
         </SidebarContent>
         <SideFooter />
         <SidebarRail />
