47ab151b6dc9e6dc9ee9894977b2848135a30235
Miguel Luis <miguelluisataideferreira68@gmail.com>
2025-01-04 16:31:27 -0300

    feat: add role for acess

--- src/shared/components/button.tsx
+++ src/shared/components/button.tsx
@@ -4,7 +4,7 @@ type ButtonProps = {
 
 export const Buttons = ({ words }: ButtonProps) => {
   return (
-    <button className="relative border py-2 px-8 rounded-lg font-medium inline-flex items-center tracking-tight align-center text-sm bg-gradient-to-b from-[#190d2e] to-[#4a208a] shadow-[0px_0px_12px_#8c45ff] hover:shadow-[0px_0px_20px_#8c45ff] transition-all duration-300">
+    <button className="relative border py-2 px-8 rounded-lg font-medium inline-flex items-center tracking-tight align-center text-sm bg-gradient-to-b from-[#190d2e] to-[#4a208a] shadow-[0px_0px_12px_#8c45ff] hover:shadow-[0px_0px_20px_#8c45ff] transition-all duration-300" role="button">
       <div className="absolute inset-0 ">
         <div className="rounded-lg border border-white/20 absolute inset-0 [mask-image:linear-gradient(to_bottom,black,transparent)]"></div>
         <div className="rounded-lg border absolute inset-0 border-white/40 [mask-image:linear-gradient(to_top,black,transparent)]"></div>
