ec42b543c511ea754c4a258187860029372eacf9
Miguel Luis <miguelluisataideferreira68@gmail.com>
2025-01-05 01:20:59 -0300

    fix: corrigindo erro de comentario

--- src/subdomains/auth/mutation/forgot-pass.tsx
+++ src/subdomains/auth/mutation/forgot-pass.tsx
@@ -15,4 +15,3 @@ export const ForgotPasswordMutation = gql`
 //     forgotPasswordd(input: { email: "miguelluisaft@gmail.com" })
 //   }
 
-// http://localhost:3000/reset-password?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI1NmQzZDMzZS04MDVjLTRmNGUtOGQ0Yi1lM2Y2Y2UxZmMyMzAiLCJpYXQiOjE3MzYwMjY2NTEsImV4cCI6MTczNjAyNzU1MX0.vRJVZCYrO8qY3rjMBDFMyTDleN8UY2T0tf0Nph6-EOg
