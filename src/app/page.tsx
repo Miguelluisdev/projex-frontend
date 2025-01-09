7c0bfd2bb4e0486ed0efcbdf3de2393e41f880a4
Miguel Luis <miguelluisataideferreira68@gmail.com>
2025-01-09 16:11:32 -0300

    refacotr: add new header component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
 import { Footer } from "@/shared/components/footer"
-import { Header } from "@/shared/components/header"
-import { CardInfo } from "../subdomains/home/index"
+
+import Header from "@/shared/components/header"
 import Faq from "@/subdomains/home/components/faq"
-import { HeroSection } from "../subdomains/home/index"
-import { LogoTicker } from "../subdomains/home/index"
 import { ProductShowcase } from "../subdomains/home//index"
-import { CallToAction } from "../subdomains/home/index"
+import {
+  CallToAction,
+  CardInfo,
+  HeroSection,
+  LogoTicker,
+} from "../subdomains/home/index"
 
 export default function Home() {
   return (
